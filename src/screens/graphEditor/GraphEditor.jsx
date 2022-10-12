0b78d86e063976eb66a182942dddfebf46afc7ae
Nikhil M Jeby <32586986+solid-droid@users.noreply.github.com>
2022-10-12 21:35:26 +0530

    Update GraphEditor.jsx

--- src/screens/graphEditor/GraphEditor.jsx
+++ src/screens/graphEditor/GraphEditor.jsx
@@ -5,11 +5,12 @@ function GraphEditor() {
   const [menu] = openMenu;
   let graph;
   let graphCanvas;
+  let editor;
  
   createEffect(async ()=>{
     if(menu.graphEditor){
         await new Promise(r => setTimeout(r, 50))
-        graphCanvas.setCanvas($('#graphCanvas')[0]);
+        graphCanvas.setCanvas('graphCanvas');
         graphCanvas.resize();
     }    
   })
@@ -19,16 +20,21 @@ function GraphEditor() {
     graphCanvas = new LGraphCanvas("#graphCanvas", graph ,{ 
       autoresize : true
      });
-    const node_const = LiteGraph.createNode("basic/const");
+    editor = new Editor(graphCanvas, graph);
+
+    let node_const = LiteGraph.createNode("basic/const");
     node_const.pos = [200,200];
     graph.add(node_const);
     node_const.setValue(4.5);
     
-    const node_watch = LiteGraph.createNode("basic/watch");
+    let node_watch = LiteGraph.createNode("basic/watch");
     node_watch.pos = [700,200];
     graph.add(node_watch);
     
     node_const.connect(0, node_watch, 0 );
+    
+    // main
+    graph.start()
     
     
   })
