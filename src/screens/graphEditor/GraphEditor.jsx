4fa2f6ac0c18f205ce52db38a2fbf455861089b7
Nikhil M Jeby <32586986+solid-droid@users.noreply.github.com>
2022-10-12 19:33:18 +0530

    litegraph + simple3D

--- src/screens/graphEditor/GraphEditor.jsx
+++ src/screens/graphEditor/GraphEditor.jsx
@@ -1,9 +1,42 @@
+import { createEffect, onMount } from 'solid-js';
 import {openMenu} from '../../scripts/store';
 
 function GraphEditor() {
   const [menu] = openMenu;
+  let graph;
+  let graphCanvas;
+ 
+  createEffect(async ()=>{
+    if(menu.graphEditor){
+        await new Promise(r => setTimeout(r, 50))
+        graphCanvas.setCanvas($('#graphCanvas')[0]);
+        graphCanvas.resize();
+    }    
+  })
+
+  onMount(() => {
+    graph = new LGraph();
+    graphCanvas = new LGraphCanvas("#graphCanvas", graph ,{ 
+      autoresize : true
+     });
+    const node_const = LiteGraph.createNode("basic/const");
+    node_const.pos = [200,200];
+    graph.add(node_const);
+    node_const.setValue(4.5);
+    
+    const node_watch = LiteGraph.createNode("basic/watch");
+    node_watch.pos = [700,200];
+    graph.add(node_watch);
+    
+    node_const.connect(0, node_watch, 0 );
+    
+    
+  })
+
   return (
-    <div>GraphEditor {menu.workspace ? 1 : 0}</div>
+    <div id="graphContainer" style='width:100%;height:100%'>
+      <canvas id='graphCanvas'/>
+    </div>
   )
 }
 
--- src/screens/simulation/Simulation.jsx
+++ src/screens/simulation/Simulation.jsx
@@ -1,6 +1,68 @@
- function Simulation(params) {
+import { createEffect, onMount } from 'solid-js'
+import {openMenu} from '../../scripts/store';
+
+import { Simple3D, box3D, ground2D, utils } from '../../scripts/simple3D';
+import * as TWEEN from '@tweenjs/tween.js';
+import { Vector3 } from '@babylonjs/core';
+
+ function Simulation() {
+
+  const [menu] = openMenu;
+  let scene;
+
+  function init(){
+
+    scene = new Simple3D('canvas3D');
+    const box = new box3D();
+    const box2 = new box3D();
+    const ground = new ground2D();
+
+    box.set.edge(true)
+       .set.opacity(0.5)
+       .set.x(4)
+       .set.z(4)
+
+    box2.set.edge(true)
+        .set.opacity(1)
+
+    ground.set.scale(10, 10)
+          .set.grid()
+    new TWEEN.Tween(box)
+             .to({height:5}, 2000)
+             .repeat(Infinity)
+             .yoyo(true)
+             .interpolation(TWEEN.Interpolation.Linear)
+             .onUpdate(() => {
+              utils.rotateAroundPivot(
+                box.mesh, 
+                new Vector3(0.5, 0, 0.5), 
+                new Vector3(0, 1, 0), 
+                0.01
+                )
+            })
+             .start();
+         
+    box2.on('longClick',(e)=> {
+        console.log('hello');
+    });
+    scene.showAxis()
+         .render([TWEEN.update]);      
+  }
+
+  createEffect(async ()=>{
+    if(menu.simulator){
+      await new Promise(r => setTimeout(r, 50));
+      if(scene){
+        scene.dispose();
+      }
+      init();
+    } else {
+
+    }
+  })
+
   return (
-    <div class={params.class}>Simulation</div>
+      <canvas style="width:100%; height:100%;" id='canvas3D' />
   )
 }
 
