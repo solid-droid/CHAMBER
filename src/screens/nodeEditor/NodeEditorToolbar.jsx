e77fd1290b49f802612e8ec35d01437f06c99d55
Nikhil M Jeby <32586986+solid-droid@users.noreply.github.com>
2023-03-26 09:54:12 +0530

    refreshButton

--- src/screens/nodeEditor/NodeEditorToolbar.jsx
+++ src/screens/nodeEditor/NodeEditorToolbar.jsx
@@ -1,10 +1,20 @@
 import {clearFlowEditor} from '../../plugins/FlowEditor/FlowScript'
+import { importJSON, updateMasterFile, clear } from '../../scripts/scripts'
+import { masterFile } from "../../scripts/store";
 
 const NodeEditorToolbar = () => {
   return (
     <div class="NodeEditorToolbar">
         <div class="toolBarButton" onClick={() => clearFlowEditor()}>
             Clear
+        </div>
+        <div class="toolBarButton" onClick={() => {
+          updateMasterFile();
+          const _masterFile = JSON.parse(JSON.stringify(masterFile));
+          clear();
+          importJSON(_masterFile);
+        }}>
+            Refresh
         </div>
         <div class="toolBarButton" onClick={() => alert('Not ready')}>
             Help
